fe0d6ad08ae4960404e501c169a8080925b0bdc5
DaLVeRS2001 <vladislav.biryukov@betting-software.online>
2026-07-04 20:54:35 +0300

    feat(db)/fix(workspace): create prisma adapter method / fix workspace webpack - build output

--- apps/auth/src/app/prisma/prisma.service.ts
+++ apps/auth/src/app/prisma/prisma.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../../generated/prisma/client';
+import { createPrismaPgAdapter } from '@pixaeron/prisma';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
-    const adapter = new PrismaPg({
-      connectionString: process.env.DATABASE_URL as string,
+    super({
+      adapter: createPrismaPgAdapter(process.env.DATABASE_URL as string),
     });
-    super({ adapter });
   }
 }
